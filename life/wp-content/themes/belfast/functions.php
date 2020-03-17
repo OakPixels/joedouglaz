39ce41771066a866ee6ee1d9c28826a13b0931ce
JoeCornish <g6doug@gmail.com>
2020-03-17 09:25:16 +0000

    bug fix

--- life/wp-content/themes/belfast/functions.php
+++ life/wp-content/themes/belfast/functions.php
@@ -4,7 +4,7 @@ function load_stylesheets()
 {
   wp_register_style('mycssname', get_template_directory_uri() . '/css/bootstrap.min.css',
     array(), false, 'all');
-  wp_enqueue_style('mycssname')
+  wp_enqueue_style('mycssname');
 }
 
 add_action('wp_enqueue_scripts', 'load_stylesheets');
