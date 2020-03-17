faf5e4a802e78d5167313468e8f85e65df2c3cbd
JoeCornish <g6doug@gmail.com>
2020-03-17 09:42:24 +0000

    jquery and linking css

--- life/wp-content/themes/belfast/functions.php
+++ life/wp-content/themes/belfast/functions.php
@@ -2,9 +2,30 @@
 
 function load_stylesheets()
 {
-  wp_register_style('mycssname', get_template_directory_uri() . '/css/bootstrap.min.css',
+  wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css',
     array(), false, 'all');
   wp_enqueue_style('mycssname');
+
+  wp_register_style('mycss', get_template_directory_uri() . '/style.css',
+    array(), false, 'all');
+  wp_enqueue_style('mycss');
+}
+add_action('wp_enqueue_scripts', 'load_stylesheets');
+
+function load_jquery()
+{
+  wp_deregister_script('jquery');
+
+  wp_enqueue_script('jquery', get_template_directory_uri() . '/js/jquery3.4.1.js',
+   '', 1, true);
+
+   add_action('wp_enqueue_scripts', 'jquery');
 }
 
-add_action('wp_enqueue_scripts', 'load_stylesheets');
+function loadjs()
+{
+  wp_register_script('myjs', get_template_directory_uri() . '/js/scripts.js',
+   '', 1, true);
+  wp_enqueue_script('myjs');
+}
+add_action('wp_enqueue_scripts', 'loadjs');
