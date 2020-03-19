feacee449cb1b9720b59ca76241273bf1b694129
JoeCornish <g6doug@gmail.com>
2020-03-19 12:18:35 +0000

    blog

--- life/wp-content/themes/belfast/archive.php
+++ life/wp-content/themes/belfast/archive.php
@@ -9,13 +9,18 @@
     <div class="card mb-4">
       <div class="card-body">
         <h3><?php the_title();?></h3>
+
+        <?php if(has_post_thumbnail()):?>
+          <img src="<?php the_post_thumbnail_url('smallest');?>" class="img-fluid">
+        <?php endif;?>
+        
         <?php the_excerpt();?>
         <a href="<?php the_permalink();?>" class="btn btn-success">Read more</a>
       </div>
     </div>
 
   <?php endwhile; endif;?>
-  
+
 </div>
 
 
--- life/wp-content/themes/belfast/single.php
+++ life/wp-content/themes/belfast/single.php
@@ -3,12 +3,12 @@
 <div class="container">
   <h1><?php the_title();?></h1>
 
-  // Import Thumbnail
+  #Import Thumbnail
   <?php if(has_post_thumbnail()):?>
-    <img src="<?php the_post_thumbnail_url('largest');?>" class="img-fluid">
+    <img src="<?php the_post_thumbnail_url('medium');?>" class="img-fluid">
   <?php endif;?>
 
-  // Import Post Content
+  #Import Post Content
   <?php if (have_posts()) : while(have_posts()) : the_post();?>
     <?php the_content();?>
   <?php endwhile; endif;?>
