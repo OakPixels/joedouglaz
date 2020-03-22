fec554e445611c0bdb1593168227cc8b317a6db1
JoeCornish <g6doug@gmail.com>
2020-03-22 16:26:19 +0000

    center title

--- life/wp-content/themes/belfast/single.php
+++ life/wp-content/themes/belfast/single.php
@@ -1,8 +1,7 @@
 <?php get_header();?>
 
 <div class="container">
-
-  <h1><?php the_title();?></h1>
+  <h1 align="center"><?php the_title();?></h1>
 
   <?php if(has_post_thumbnail()):?>
     <img src="<?php the_post_thumbnail_url('medium');?>" class="img-fluid">
